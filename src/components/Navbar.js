9943291d0abe4a55dcb5d313792d451949bc6d77
Bongz Podolski <ntshingilab@outlook.com>
2023-10-01 10:11:23 +0200

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <Link className="navLink" to="/">
+      <img src="" alt="Logo" />
+      </Link>      
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+    return (
+        <div className="navbar">
+            <p>This is the Home Page</p>
+        </div>
+    );
+}
+
+export default Home;
