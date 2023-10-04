c10d6d4a2fe7ec5fe1313e2811a63b930486aab3
Bongz Podolski <ntshingilab@outlook.com>
2023-10-04 21:15:44 +0200

    initial menu content

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -2,8 +2,10 @@ import React from "react";
 
 function Menu() {
     return (
-        <div>
-            <h4>This is the menu page</h4>
+        <div className="hero-section">
+            <h2 className="hero-title">FreshGo Menu</h2>
+            <h4 className="hero-subtitle">Our menu features a wide variety of fresh-baked goods. We also offer a variety of specialty items, such as gluten-free and vegan baked goods.</h4>
+            <p className="hero-description">Order online today for pickup or delivery!</p>
         </div>
     );
 }
